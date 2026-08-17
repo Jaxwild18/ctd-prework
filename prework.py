23ec437aed247f52a6965e7600aaa30d661340cc
Jaxwild18 <jaxwild18@gmail.com>
2026-08-16 21:15:13 -0500

    Create prework.py

---
+++ prework.py
@@ -0,0 +1,12 @@
+# This program has Scorpion fight Sub-Zero in Mortal Kombat.
+
+fighter = "Scorpion"
+health = 0
+opponent = "Sub-Zero"
+
+print(fighter, "will fight", opponent)
+
+if health == 0:
+    print(fighter, "is defeated!")
+else:
+    print(fighter, "is in a groggy state... FATALITY!")
